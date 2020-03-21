07466e704bc51eca599f73be84ac2330e9c8d6a7
MathieuVeber <43341681+MathieuVeber@users.noreply.github.com>
2020-03-21 18:03:12 +0100

    Config with other website CORS done

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ dotenv.config();
 // Express
 var express = require('express');
 var app = express();
+app.set('port', process.env.PORT || 3000);
 
 // Routes
 var indexRouter = require('./routes/index');
@@ -28,7 +29,27 @@ mongoose.connect(process.env.DB_CONNECT,{}, () => console.log('Successfully conn
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors());
+// Cross domain
+// app.use(cors());
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
