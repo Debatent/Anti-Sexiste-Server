3b56de2b60a31abf03ac2d8ab03ee0fee7f38899
Debatent <alexandreadrien@orange.fr>
2020-04-01 15:42:01 +0200

    Add another function to manage the admins

--- routes/admin-management.js
+++ routes/admin-management.js
@@ -19,12 +19,22 @@ router.get('/admins', async function(req, res, next) {
         .then(data => res.status(200).json(data))
         .catch(err => {res.status(400).json({message: err})})
 });
-/*UPDATE an admin*/
-router.patch('/admins/:idUser/',async function(req,res,next){
+/*ADD an admin*/
+router.put('/admins',async function(req,res,next){
+    const userExist = await User.findOne({pseudo:req.body.pseudo});
+    if (!userExist) return res.status(400).send('User doesn\'t exists');
+    
+    User.findOneAndUpdate({pseudo:req.body.pseudo},{isAdmin: true})
+        .then(data => res.status(200).json(data))
+        .catch(err => {res.status(400).json({message: err})})
+});
+
+/*DELETE an admin*/
+router.delete('/admins/:idUser/',async function(req,res,next){
     const userExist = await User.findById(req.params.idUser);
     if (!userExist) return res.status(400).send('User doesn\'t exists');
     
-    User.findByIdAndUpdate(req.params.idUser,{isAdmin: req.body.value})
+    User.findByIdAndUpdate(req.params.idUser,{isAdmin: false})
         .then(data => res.status(200).json(data))
         .catch(err => {res.status(400).json({message: err})})
 });
