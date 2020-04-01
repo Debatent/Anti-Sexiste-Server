5940d7b418a12f1fa90de3e9ecbcc78e83d1df71
Debatent <alexandreadrien@orange.fr>
2020-04-01 11:36:36 +0200

    Change Api to add Admin via the web app

---
+++ routes/admin-management.js
@@ -0,0 +1,30 @@
+// Express
+const express = require('express');
+const router = express.Router();
+
+// Middlewares
+const {wrapAsync} = require('./middlewares/async');
+const {hardAdmin} = require('./middlewares/admin');
+
+// Model
+const User = require('../models/User');
+
+/* Restricted admin area */
+router.use('/admins', wrapAsync(hardAdmin));
+
+router.get('/admins', async function(req, res, next) {
+    User.find({isAdmin:true})
+        .select({pseudo:1})
+        .exec()
+        .then(data => res.status(200).json(data))
+        .catch(err => {res.status(400).json({message: err})})
+});
+/*UPDATE an admin*/
+router.patch('/admins/:idUser/',async function(req,res,next){
+    const userExist = await User.findById(req.params.idUser);
+    if (!userExist) return res.status(400).send('User doesn\'t exists');
+    
+    User.findByIdAndUpdate(req.params.idUser,{isAdmin: req.body.value})
+        .then(data => res.status(200).json(data))
+        .catch(err => {res.status(400).json({message: err})})
+});
